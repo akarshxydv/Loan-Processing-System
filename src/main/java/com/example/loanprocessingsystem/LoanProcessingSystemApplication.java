a4dd38e2056a1b58b65686e6f53bcc0340540aa1
Akarsh Yadav <akarshxydv@outlook.in>
2024-01-21 15:20:55 +0530

    initial

---
+++ src/main/java/com/example/loanprocessingsystem/LoanProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.loanprocessingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanProcessingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanProcessingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loanprocessingsystem/LoanProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanprocessingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanProcessingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
